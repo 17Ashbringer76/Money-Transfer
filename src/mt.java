6aee565429ba25aca15c7d6c0df00a3658d96380
17Ashbringer76 <77210553+17Ashbringer76@users.noreply.github.com>
2021-02-07 23:41:27 +0300

    Update mt.java

--- src/mt.java
+++ src/mt.java
@@ -2,7 +2,7 @@ public class mt {
     public static void mt(String[]args) {
     int balance = 2_000_000_000;
     int transfer = 500_000_000;
-    int total = balance - transfer;
+    int total = balance + transfer;
     System.out.println(total);
 }
 }
