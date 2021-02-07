4a84aff2867552ffa1bc87a227ef43215ddb8208
17Ashbringer76 <77210553+17Ashbringer76@users.noreply.github.com>
2021-02-07 23:35:32 +0300

    Add files via upload

---
+++ src/mt.java
@@ -0,0 +1,8 @@
+public class mt {
+    public static void mt(String[]args) {
+    int balance = 2_000_000_000;
+    int transfer = 500_000_000;
+    int total = balance - transfer;
+    System.out.println(total);
+}
+}
